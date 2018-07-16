f9f43fc989dfccdff545c971ece9ecebe9b183ca
SasakiYuki <wacorggen@gmail.com>
2018-07-17 01:08:45 +0900

    シングルHuaweiボタンを追加

--- app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/custom_view/RaccoonMaterialButton.kt
+++ app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/custom_view/RaccoonMaterialButton.kt
@@ -1,4 +1,16 @@
 package wacode.yamada.yuki.nempaymentapp.view.custom_view
 
-class RaccoonMaterialButton {
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.view.View
+import wacode.yamada.yuki.nempaymentapp.R
+
+class RaccoonMaterialButton(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : ConstraintLayout(context, attrs, defStyleAttr) {
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context?) : this(context, null, 0)
+
+    init {
+        View.inflate(context, R.layout.view_raccoon_material_button, this)
+    }
 }
