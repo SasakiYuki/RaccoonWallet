c56928479c0e3a6bb1668689a90616ce664af7d9
SasakiYuki <wacorggen@gmail.com>
2018-07-16 14:24:24 +0900

    Huaweiボタン雛型

---
+++ app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/activity/SampleActivity.java
@@ -0,0 +1,15 @@
+package wacode.yamada.yuki.nempaymentapp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wacode.yamada.yuki.nempaymentapp.R;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
