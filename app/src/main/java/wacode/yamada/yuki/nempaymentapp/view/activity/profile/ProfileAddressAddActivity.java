e2791dbfc8d8f96c8c2e6c0e036ddb86671ab95d
SasakiYuki <wacorggen@gmail.com>
2018-07-23 01:18:38 +0900

    ProfileAddressAddActivityを追加

---
+++ app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/activity/profile/ProfileAddressAddActivity.java
@@ -0,0 +1,15 @@
+package wacode.yamada.yuki.nempaymentapp.view.activity.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wacode.yamada.yuki.nempaymentapp.R;
+
+public class ProfileAddressAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_address_add);
+    }
+}
