db931fc1d232596203b85b8a946b26b7a4500b8b
SasakiYuki <wacorggen@gmail.com>
2018-08-14 22:53:23 +0900

    ツールバー上に確定ボタンを追加　EditText属性をCardViewの中に

--- app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/activity/profile/ProfileAddressAddActivity.kt
+++ app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/activity/profile/ProfileAddressAddActivity.kt
@@ -2,21 +2,26 @@ package wacode.yamada.yuki.nempaymentapp.view.activity.profile
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
 import wacode.yamada.yuki.nempaymentapp.R
+import wacode.yamada.yuki.nempaymentapp.view.activity.BaseActivity
 
-class ProfileAddressAddActivity : AppCompatActivity() {
-
+class ProfileAddressAddActivity : BaseActivity() {
+    override fun setLayout() = R.layout.activity_profile_address_add
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_address_add)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        setToolBarBackButton()
+        setTitle(R.string.profile_address_add_activity_title)
     }
 
     companion object {
-        fun createIntent(context:Context): Intent {
-            val intent = Intent(context,ProfileAddressAddActivity::class.java)
+        fun createIntent(context: Context): Intent {
+            val intent = Intent(context, ProfileAddressAddActivity::class.java)
             return intent
         }
     }
