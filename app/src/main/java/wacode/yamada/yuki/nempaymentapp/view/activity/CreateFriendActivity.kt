64a8ba0a4174ba8cb010fd58ef08810198d58d26
Soma.Kato <riberd.puzzle@gmail.com>
2018-08-18 17:24:50 +0900

    #Fix 挙動の修正

--- app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/activity/CreateFriendActivity.kt
+++ app/src/main/java/wacode/yamada/yuki/nempaymentapp/view/activity/CreateFriendActivity.kt
@@ -1,8 +1,14 @@
 package wacode.yamada.yuki.nempaymentapp.view.activity
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.view.ViewPager
+import android.view.View
+import com.isseiaoki.simplecropview.CropImageView
 import kotlinx.android.synthetic.main.activity_create_friend.*
 import wacode.yamada.yuki.nempaymentapp.R
 import wacode.yamada.yuki.nempaymentapp.view.adapter.SimpleViewPagerAdapter
@@ -15,20 +21,68 @@ class CreateFriendActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupViews()
+        setupViewPager()
+        setupSelectIconButton()
     }
 
-    private fun setupViews() {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_DROP_IMAGE -> {
+                    data?.let {
+                        val uriString = it.getStringExtra(CropImageActivity.PARAM_INTENT_RESULT_URI)
+                        val icon = MediaStore.Images.Media.getBitmap(contentResolver, Uri.parse(uriString))
+                        iconImageView.setImageBitmap(icon)
+                        iconImageView.tag = uriString
+
+                        selectIconRootView.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupSelectIconButton() {
+        iconImageView.setOnClickListener {
+            startActivityForResult(CropImageActivity.createIntent(this, CropImageView.CropMode.CIRCLE_SQUARE), REQUEST_CODE_DROP_IMAGE)
+        }
+    }
+
+    private fun setupViewPager() {
         val list = ArrayList<BaseFragment>()
         list.add(CreateFriendWalletFragment.newInstance())
         list.add(CreateFriendAddressFragment.newInstance())
 
         val adapter = SimpleViewPagerAdapter(this, list, supportFragmentManager)
-        createFriendViewPager.adapter = adapter
-        createFriendTabLayout.setupWithViewPager(createFriendViewPager)
+        createAddressBookViewPager.adapter = adapter
+        createAddressBookTabLayout.setupWithViewPager(createAddressBookViewPager)
+
+        createAddressBookViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                        walletRootButton.visibility = View.VISIBLE
+                        addressRootButton.visibility = View.GONE
+                    }
+                    1 -> {
+                        walletRootButton.visibility = View.GONE
+                        addressRootButton.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
     }
 
     companion object {
+        private const val REQUEST_CODE_DROP_IMAGE = 1000
+
         fun createIntent(context: Context): Intent {
             val intent = Intent(context, CreateFriendActivity::class.java)
             return intent
